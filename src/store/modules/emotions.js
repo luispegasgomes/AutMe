881d3b3ef7bbece7a0038f1d13626f39de41a144
WNRPTR <pedrique2002@gmail.com>
2022-01-06 17:47:19 +0000

    adding game images

--- src/store/modules/emotions.js
+++ src/store/modules/emotions.js
@@ -47,19 +47,9 @@ export default {
           "https://images.emojiterra.com/google/android-11/512px/1f632.png",
       },
       {
-        name: "Desespero",
-        imgUrl:
-          "https://images.emojiterra.com/google/android-11/512px/1f631.png",
-      },
-      {
         name: "Dúvida",
         imgUrl:
           "https://images.emojiterra.com/google/android-11/512px/1f914.png",
-      },
-      {
-        name: "Fúria",
-        imgUrl:
-          "https://images.emojiterra.com/google/android-11/512px/1f624.png",
       },
       {
         name: "Indiferença",
@@ -90,6 +80,114 @@ export default {
         name: "Vergonha",
         imgUrl:
           "https://images.emojiterra.com/google/android-11/512px/1f633.png",
+      },
+
+
+      //imagens para o jogo
+      {
+        name: "Vergonha",
+        imgUrl:
+          "https://image.freepik.com/free-photo/smiling-man-facepalm-after-embarrassing-joke_176420-18019.jpg",
+      },
+      {
+        name: "Vergonha",
+        imgUrl:
+          "https://image.freepik.com/free-photo/embarrassed-man-facepalm-smiling_176420-19598.jpg",
+      },
+      {
+        name: "Vergonha",
+        imgUrl:
+          "https://image.freepik.com/fotos-gratis/cara-atraente-em-t-shirt-elegante-fazendo-gesto-de-palma-do-rosto-rindo-sentindo-se-envergonhado-jovem-emocional-cobrindo-os-olhos-por-causa-da-dor-vergonha-tristeza-perda-vergonha-ou-estresse_343059-1526.jpg",
+      },
+      {
+        name: "Tristeza",
+        imgUrl:
+          "https://services.meteored.com/img/article/por-que-algumas-pessoas-sao-mais-tristes-no-inverno-348391-1_768.jpg",
+      },
+      {
+        name: "Tristeza",
+        imgUrl:
+          "https://conteudo.imguol.com.br/c/entretenimento/6b/2018/01/13/pessoa-triste-homem-triste-homem-refletindo-1515870196828_v2_450x337.jpg",
+      },
+      {
+        name: "Tristeza",
+        imgUrl:
+          "https://images.freejpg.com.ar/400/2606/man-sad-portrait-hood-person-depression-emotion-F100028602.jpg",
+      },
+      {
+        name: "Raiva",
+        imgUrl:
+          "https://gooutside.com.br/wp-content/uploads/sites/3/2019/03/raiva-exercicio.jpg",
+      },
+      {
+        name: "Raiva",
+        imgUrl:
+          "https://cdn-images-1.medium.com/max/900/1*idU75tMkx7bqTTBF8EtGCA.jpeg",
+      },
+      {
+        name: "Raiva",
+        imgUrl:
+          "https://revistaregional.com.br/site/wp-content/uploads/2018/11/raiva.jpg",
+      },
+      {
+        name: "Nojo",
+        imgUrl:
+          "https://img.vixdata.io/pd/jpg-large/pt/sites/default/files/n/nojo-repulsa-rejeicao-0618-1400x800.jpg",
+      },
+      {
+        name: "Nojo",
+        imgUrl:
+          "https://conteudo.imguol.com.br/c/entretenimento/f4/2018/11/29/ranco-nojo-1543515979792_v2_4x3.jpg",
+      },
+      {
+        name: "Nojo",
+        imgUrl:
+          "https://pm1.narvii.com/6719/af1f22c3239d5418e573878363e588b30af833bb_hq.jpg",
+      },
+      {
+        name: "Nervosismo",
+        imgUrl:
+          "https://melhorcomsaude.com.br/wp-content/uploads/2014/10/nervos1.jpg",
+      },
+      {
+        name: "Nervosismo",
+        imgUrl:
+          "https://colunaitalo.com.br/upload/artigos/1229/CAPA_SUPERA_view.jpg",
+      },
+      {
+        name: "Nervosismo",
+        imgUrl:
+          "https://guiadoestudante.abril.com.br/wp-content/uploads/sites/4/2016/11/ansiedade-vestibular.jpg",
+      },
+      {
+        name: "Indiferença",
+        imgUrl:
+          "https://st2.depositphotos.com/1258191/8822/i/600/depositphotos_88229574-stock-photo-confused-man-shrugging.jpg",
+      },
+      {
+        name: "Indiferença",
+        imgUrl:
+          "https://st2.depositphotos.com/4196725/7818/i/600/depositphotos_78188520-stock-photo-boring-businessman-yawning.jpg",
+      },
+      {
+        name: "Indiferença",
+        imgUrl:
+          "https://image.freepik.com/free-photo/silly-unaware-guy-shrugging-smiling-know-anything-being-clueless_176420-21743.jpg",
+      },
+      {
+        name: "Dúvida",
+        imgUrl:
+          "https://i1.wp.com/cienciafe.miguelpanao.com/wp-content/uploads/2017/12/1BA89864-FB05-458C-A5CD-6304C0B8F904.jpeg?resize=426%2C282&ssl=1",
+      },
+      {
+        name: "Dúvida",
+        imgUrl:
+          "https://sindilegis.org.br/wp-content/uploads/2018/11/img-05-07-2016-577c1944c2c4c.jpg",
+      },
+      {
+        name: "Dúvida",
+        imgUrl:
+          "https://blog.meskle.com.br/wp-content/uploads/2020/09/As-principais-duvidas-sobre-celulite-homens.jpg",
       },
     ],
   },
